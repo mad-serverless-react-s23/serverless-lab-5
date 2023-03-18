00c37fe7964a8470b23442de040ff95522b4d723
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-17 21:05:12 -0500

    added first book logic
    just following the book. If it fails, I know who to blame

--- amplify/backend/function/lab5crypto/src/app.js
+++ amplify/backend/function/lab5crypto/src/app.js
@@ -1,13 +1,3 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -25,6 +15,16 @@ app.use(function(req, res, next) {
   next()
 });
 
+app.get('/coins', function(req, res) {
+  const coins = [
+    { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
+    { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
+    { name: 'Litecoin', symbol: 'LTC', price_usd: "150" }
+  ]
+  res.json({
+    coins
+  })
+});
 
 /**********************
  * Example get method *
