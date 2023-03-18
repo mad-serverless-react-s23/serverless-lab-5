62f9e136e06a4f19ef4323da04dd06faebe40826
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-17 21:38:35 -0500

    placed code as directed by book
    whatever fails it's O'Reilly's fault...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
 
 function App() {
+  // creates coins variable and sets as empty array
+  const [coins, updateCoins] = useState([]);
+
+  // define func to all API
+  async function fetchCoins() {
+    const data = await API.get('lab5api', '/coins');
+    updateCoins(data.coins);
+  };
+
+  // call fetch func on component load
+  useEffect(() => {
+    fetchCoins()
+  }, []);
+
+  // set up the display...
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
