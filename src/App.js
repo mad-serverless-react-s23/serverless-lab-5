f4df71295a4aec992260e0ad93d7329fe7efae60
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-19 16:40:36 -0500

    throwing errors like confetti...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 const App = () => {
   // creates coins variable and sets as empty array
@@ -14,21 +17,18 @@ const App = () => {
 
   // call fetch func on component load ONCE
   useEffect(() => {
-    fetchCoins()
+    fetchCoins();
   }, []);
   // ^^ empty array added to restrict code to run just once, the first time component loads
-
   // set up the display...
   return (
     <div className="App">
-      {
-        coins.map((coin, index) => (
-          <div key={index}>
-            <h2>{coin.name} - {coin.symbol}</h2>
-            <h5>${coin.price_usd}</h5>
-          </div>
-        ))
-      }
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>{coin.name} - {coin.symbol}</h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   );
 }
