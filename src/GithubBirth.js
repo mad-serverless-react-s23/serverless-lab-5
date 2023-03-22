e842e397008be97639028e9a4ded8c14b7ebdad7
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-22 12:26:49 -0500

    function base
    the function is written and the placeholder still works...

--- src/GithubBirth.js
+++ src/GithubBirth.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
 
-export const GithubBirth = () => (
+export const GithubBirth = () => {
+    const [birth, updateBirth] = useState([]);
+
+    const fetchBirth = async() => {
+        const data = await API.get('lab5api', '/born');
+        updateBirth(data.birth);
+    }
+
+    useEffect(() => {
+        fetchBirth()
+    }, []);
+    
+    return (
     <h3>This message might work, let's see...</h3>
-)
+)}
