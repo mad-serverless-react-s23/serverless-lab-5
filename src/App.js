cf9261fb1acc95739590c233881b8f08bc275e24
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-22 16:11:09 -0500

    Coins to new component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { API } from 'aws-amplify';
 import { GithubBirth } from './GithubBirth';
+import { Coins } from './Coins';
 
 const App = () => {
-  const [coins, updateCoins] = useState([]);
+  
 
-  const [input, updateInput] = useState({ limit: 5, start: 0});
-
-  const updateInputValues = (type, value) => updateInput(
-    { ...input, [type]: value }
-  );
-
-  const fetchCoins = async() => {
-    updateLoading(true);
-    const { start, limit } = input;
-    const data = await API.get('lab5api', `/coins?start=${start}&limit=${limit}`);
-    updateCoins(data.coins);
-    updateLoading(false);
-  };
-
-  useEffect(() => {
-    fetchCoins()
-  }, []);
-
-  const [loading, updateLoading] = useState(true);
+  
 
   return (
     <div className="App">
-      <input
-        onChange={e => updateInputValues('start', e.target.value)}
-        placeholder="start"
-      />
-      <hr/>
-      <input
-        onChange={e => updateInputValues('limit', e.target.value)}
-        placeholder="limit"
-      />
-      <hr/>
-      <button
-        onClick={fetchCoins}
-      >Fetch Coins</button>
-      <hr/>
-      {loading && <h2>Loading your request...</h2>}
-      {
-        !loading && coins.map((coin, index) => (
-        <div key={index}>
-          <h2>{coin.name} - {coin.symbol}</h2>
-          <h4>${coin.price_usd}</h4>
-        </div>
-      ))}
+      <Coins />
       <GithubBirth />
     </div>
   );
---
+++ src/Coins.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+
+export const Coins = () => {
+    const [coins, updateCoins] = useState([]);
+
+    const [input, updateInput] = useState({ limit: 5, start: 0});
+
+  const updateInputValues = (type, value) => updateInput(
+    { ...input, [type]: value }
+  );
+
+  const fetchCoins = async() => {
+    updateLoading(true);
+    const { start, limit } = input;
+    const data = await API.get('lab5api', `/coins?start=${start}&limit=${limit}`);
+    updateCoins(data.coins);
+    updateLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCoins()
+  }, []);
+
+  const [loading, updateLoading] = useState(true);
+
+  return (
+    <>
+        <input
+        onChange={e => updateInputValues('start', e.target.value)}
+        placeholder="start"
+      />
+      <hr/>
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      <hr/>
+      <button
+        onClick={fetchCoins}
+      >Fetch Coins</button>
+      <hr/>
+      {loading && <h2>Loading your request...</h2>}
+      {
+        !loading && coins.map((coin, index) => (
+        <div key={index}>
+          <h2>{coin.name} - {coin.symbol}</h2>
+          <h4>${coin.price_usd}</h4>
+        </div>
+      ))}
+    </>
+  )
+}
