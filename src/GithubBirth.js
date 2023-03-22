7d5d4f972a233bea26b032b35325c78589374b7a
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-22 12:21:40 -0500

    that works
    imports listed, next to write a function

--- src/GithubBirth.js
+++ src/GithubBirth.js
@@ -1,1 +1,6 @@
-export const GithubBirth = () => <h3>This message displays</h3>
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+
+export const GithubBirth = () => (
+    <h3>This message might work, let's see...</h3>
+)
